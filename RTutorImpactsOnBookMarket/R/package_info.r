7dcf0dce7d0f674702aa315e29d888a8a5cf5829
LeonardPoehls <leonard.poehls@uni-ulm.de>
2023-06-05 15:05:49 +0200

    Initial Commit

---
+++ RTutorImpactsOnBookMarket/R/package_info.r
@@ -0,0 +1,9 @@
+ps.pkg.info = function() {
+  # Name of this package
+  package = "RTutorImpactsOnBookMarket"
+  
+  # Name of problem sets in the package
+  ps = c("The Impacts of reviews and Ratings on Book Market")
+  
+  list(package=package, ps = ps)
+}
